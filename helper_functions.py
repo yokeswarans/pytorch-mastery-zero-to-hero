dd68bad137108c299528e4c52e8dfc3974afda4d
yokeswaran <yokeswaran1718@gmail.com>
2026-05-05 21:30:25 +0530

    Altered helper function

--- helper_functions.py
+++ helper_functions.py
@@ -37,8 +37,6 @@ def walk_through_dir(dir_path):
 
 def plot_decision_boundary(model: torch.nn.Module, X: torch.Tensor, y: torch.Tensor):
     """Plots decision boundaries of model predicting on X in comparison to y.
-
-    Source - https://madewithml.com/courses/foundations/neural-networks/ (with modifications)
     """
     # Put everything to CPU (works better with NumPy + Matplotlib)
     model.to("cpu")
